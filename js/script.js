8aba10ac3f365f921d660230d4bf5a4d2b1b11a3
Claudia <cmplg20@gmail.com>
2019-10-27 22:03:35 -0300

    Versao 5

--- js/script.js
+++ js/script.js
@@ -1,8 +1,43 @@
+const listas = document.querySelectorAll('ul') 
 const inputToDo = document.querySelector('div.header input')
 const btAdd= document.querySelector('div.header button')
 const toDo = document.querySelector('#toDo');
 const doing = document.querySelector('#doing');
 const done = document.querySelector('#done');
+
+const save = () => {
+
+    let listasTransformadas = Array.from(listas)
+        .map(lista => {
+            return {
+                nomeLista: lista.getAttribute('id'),
+                itens: Array.from(lista.children)
+                    .map(filho => {
+                        const [descricao] = filho.querySelectorAll('p')
+                        return {
+                            descricao: descricao.textContent
+                        }
+                    })
+            }
+        })
+
+    localStorage.setItem('lista', JSON.stringify(listasTransformadas))
+}
+
+const load = () => {
+    let listas = JSON.parse(localStorage.getItem('lista'))
+    if (listas) {
+        listas.forEach(lista => {
+            let listaElemento = document.querySelector(`#${lista.nomeLista}`)
+            lista.itens.forEach(item => {
+                criarItem(item.descricao, listaElemento)
+            })
+
+        })
+
+    }
+
+}
 
 const excluirItem = (item) => {
     let animation = item.animate([{
@@ -19,49 +54,57 @@ const excluirItem = (item) => {
     }
 }
 
-const criarItem = (descricao) => {
+const criarItem = (descricao, toDo) => {
     const li = document.createElement('li')
     const button = document.createElement('button')
     const img = document.createElement('i')
+    const pDescricao = document.createElement('p')
 
     img.setAttribute('class','fa fa-angle-double-right')
     button.appendChild(img)
 
-    inputToDo.textContent = descricao
-    li.append(descricao,' ', button)
+    pDescricao.textContent = descricao
+    pDescricao.classList.add('descricao')
 
-    button.onclick = () => irDoing(li, descricao)
+    li.append(pDescricao, button)
+
+    button.onclick = () => irDoing(li, pDescricao)
 
     toDo.appendChild(li)
+
+    save()
 }
 
 const adicionarItem = () => {
     if (inputToDo.value) {
-        criarItem(inputToDo.value)
+        criarItem(inputToDo.value, toDo)
         inputToDo.value = ''
+        save()
     } else {
         alert('Preencha o campo!')
     }
+    
 }
 
 btAdd.onclick = adicionarItem
 
-const irDoing = (item, descricao) => {
+const irDoing = (item, pDescricao) => {
     const li = document.createElement('li')
     const button = document.createElement('button')
     const btDoing = document.createElement('i')
   
     btDoing.setAttribute('class', 'fa fa-angle-double-right')
     button.appendChild(btDoing)
-    button.onclick = () => irDone(li, descricao)
+    button.onclick = () => irDone(li, pDescricao)
   
-    li.append(descricao,' ', button)
+    li.append(pDescricao, button)
   
     doing.appendChild(li)
     toDo.removeChild(item)
+    save()
   }
 
-const irDone = (item, descricao) => {
+const irDone = (item, pDescricao) => {
     const li = document.createElement('li')
     const button = document.createElement('button')
     const btDone = document.createElement('i')
@@ -70,8 +113,11 @@ const irDone = (item, descricao) => {
     button.appendChild(btDone)
     button.onclick = () => excluirItem(li)
   
-    li.append(descricao,' ', button)
+    li.append(pDescricao, button)
   
     done.appendChild(li)
     doing.removeChild(item)
+    save()
   }
+
+  load()
